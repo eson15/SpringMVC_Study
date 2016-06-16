8ceb82561f3718680d333bbcd225f4ceb7e0a177
nishengwu <ni_shengwu@126.com>
2016-06-16 13:10:56 +0800

    走进SpringMVC的世界

---
+++ src/ssm/controller/ItemsController1.java
@@ -0,0 +1,48 @@
+package ssm.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import ssm.po.Items;
+
+public class ItemsController1 implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		//实际中是调用service查找数据库，查询商品列表，这里直接使用静态数据来模拟了
+		List<Items> itemsList = new ArrayList<Items>();
+		//向list中填充静态数据
+		Items items_1 = new Items();
+		items_1.setName("联想笔记本");
+		items_1.setPrice(6000f);
+		items_1.setDetail("ThinkPad T430 联想笔记本电脑！");
+		
+		Items items_2 = new Items();
+		items_2.setName("苹果手机");
+		items_2.setPrice(5000f);
+		items_2.setDetail("iPhone6苹果手机！");
+		
+		itemsList.add(items_1);
+		itemsList.add(items_2);
+		
+		//返回ModelAndView
+		ModelAndView modelAndView = new ModelAndView();
+		
+		//相当于request的setAttribute，在jsp页面中通过itemsList取数据
+		modelAndView.addObject("itemaList", itemsList);
+		
+		//指定视图
+		modelAndView.setViewName("/WEB-INF/jsp/items/itemsList.jsp");
+		
+		return modelAndView;
+	}
+
+}
